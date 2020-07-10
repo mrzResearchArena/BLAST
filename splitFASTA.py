f87aa342c2c95fed4eb97b284e127977692b0021
Rafsanjani, Muhammod <rafsanjani.muhammod@gmail.com>
2020-07-11 00:52:10 +0600

    SplitFASTA

---
+++ splitFASTA.py
@@ -0,0 +1,12 @@
+File = '/home/mrz/MyDrive/Education/Bioinformatics/sequences.txt'
+
+
+from Bio import SeqIO
+
+C= 1
+for record in SeqIO.parse(File, 'fasta'):
+    openFile = open(str(C) + '.fasta', 'w')
+    SeqIO.write(record, openFile, 'fasta')
+    C += 1
+#end-for
+
