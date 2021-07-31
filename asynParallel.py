7bf5262fd01d4dd475a26bc491e1057e478f4801
Rafsanjani, Muhammod <rafsanjani.muhammod@gmail.com>
2021-07-31 15:37:26 +0600

    Update asynParallel.py

--- asynParallel.py
+++ asynParallel.py
@@ -1,7 +1,13 @@
 ### Load Essential Files
 database = '/home/learning/mrzResearchArena/NR/nr'   # Please, set path where "nr" database directory is located.
 PSSM = '/home/learning/mrzResearchArena/PSSM'        # Please, set path where PSSM directory is located.
-core = 8                                             # multiprocessing.cpu_count(). Please don't use the maximum core.
+###
+
+###
+# Parameters:
+core = 8            # multiprocessing.cpu_count(). Please don't use the maximum core.
+iteration = 3      # If we increase the number iteration, then we will get the good quality of PSSM.
+evalue = 0.001      # E-value
 ###
 
 ### Load Essential Modules
@@ -17,7 +23,7 @@ os.chdir(PSSM) # PSSM file will generate in PSSM directory.
 ### Generate PSSM
 def runPSIBLAST(file):
     try:
-        os.system('/home/learning/ncbi-blast-2.10.1+/bin/psiblast -query {} -db {} -out {}.out -num_iterations 3 -out_ascii_pssm {}.pssm -inclusion_ethresh 0.001 -comp_based_stats 0 -num_threads 1'.format(file, database, file, file))
+        os.system('/home/learning/ncbi-blast-2.10.1+/bin/psiblast -query {} -db {} -out {}.out -num_iterations {} -out_ascii_pssm {}.pssm -inclusion_ethresh {} -comp_based_stats 0 -num_threads 1'.format(file, database, file, iteration, file, evalue))
     except:
         print('PSI-BLAST is error for the sequence {}!'.format(file))
         return '{}, is error.'.format(file)
