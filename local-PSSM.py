09b525cbaffd9ab22b31b25cb6c67b75edcc7ff3
Rafsanjani, Muhammod <rafsanjani.muhammod@gmail.com>
2021-08-02 05:45:02 +0600

    Add files via upload

--- local-PSSM.py
+++ local-PSSM.py
@@ -2,14 +2,11 @@ import glob
 import os
 
 ### Parameters:
-iteration = 3       # If we increase the number iteration, then we will get the good quality of PSSM.
-evalue    = 0.001   # E-value
+iteration = 10       # If we increase the number iteration, then we will get the good quality of PSSM.
+evalue    = 0.001    # E-value
 ###
 
-C = 0
+
 for file in sorted(glob.glob('*.fasta')):
-    # print(file)
-    if C==50: break
     os.system('/home/rafsanjani/Dropbox/CompBio/PSI-BLAST/ncbi-blast-2.12.0+/bin/psiblast -query {} -db Pluto -num_iterations={} -evalue={} -out psiblastout.txt -out_ascii_pssm {}.pssm'.format(file, iteration, evalue, file))
-    C = C+1
 #end-for
