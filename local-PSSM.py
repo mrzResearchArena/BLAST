fb28726a4033481221b5ea8c7374ae92353a84b9
Rafsanjani, Muhammod <rafsanjani.muhammod@gmail.com>
2021-08-02 04:27:44 +0600

    Add files via upload

--- local-PSSM.py
+++ local-PSSM.py
@@ -9,7 +9,7 @@ evalue    = 0.001   # E-value
 C = 0
 for file in sorted(glob.glob('*.fasta')):
     # print(file)
-    if C==10: break
-    os.system('/home/rafsanjani/Dropbox/CompBio/PSI-BLAST/ncbi-blast-2.12.0+/bin/psiblast -query {} -db Pluto -num_iterations={} -evalue={} -out psiblastout.txt -out_ascii_pssm {}-.PSSM -save_each_pssm'.format(file, iteration, evalue, file))
+    if C==50: break
+    os.system('/home/rafsanjani/Dropbox/CompBio/PSI-BLAST/ncbi-blast-2.12.0+/bin/psiblast -query {} -db Pluto -num_iterations={} -evalue={} -out psiblastout.txt -out_ascii_pssm {}.pssm'.format(file, iteration, evalue, file))
     C = C+1
 #end-for
