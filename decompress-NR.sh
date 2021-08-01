fa5c2020c45ced2266415459adb306b504604a89
Rafsanjani, Muhammod <rafsanjani.muhammod@gmail.com>
2021-08-01 22:53:01 +0600

    Update and rename directExtract.sh to decompress-NR.sh

--- decompress-NR.sh
+++ decompress-NR.sh
@@ -1,4 +1,6 @@
-for i in $(seq 1 1 3); do
+n=38   ### If the number of the segment is n, then we will use n-1.
+
+for i in $(seq 0 1 $n); do
     if [ $i -lt 10 ]; then
         tar -xvzf nr.0$i.tar.gz
     else
